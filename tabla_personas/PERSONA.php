6ddd50daa16b82917f580ee24b655cbd76e5c7d5
Gorila71 <adribid35@gmail.com>
2025-03-11 21:02:54 -0600

    Create PERSONA.php

---
+++ tabla_personas/PERSONA.php
@@ -0,0 +1,43 @@
+<?php
+class Persona {
+    private $nombre;
+    private $FecNac;
+    private $Tel;
+
+    public function __construct($nombre, $FecNac, $Tel){
+        $this->nombre = $nombre;
+        $this->FecNac = $FecNac; 
+        $this->Tel = $Tel;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function setNombre($value){
+        $this->nombre = $value; 
+    }
+
+    public function getFecNac(){
+        return $this->FecNac;
+    }
+    public function setFecNac($value){
+        $this->FecNac = $value; 
+    }
+
+    public function getTel(){
+        return $this->Tel;
+    }
+    public function setTel($value){
+        $this->Tel = $value; 
+    }
+
+    public function getEdad(){
+        $fechaNacimiento = new DateTime($this->getFecNac()); 
+        $hoy = new DateTime();
+        $edad = $hoy->diff($fechaNacimiento);
+        return $edad->y; 
+    }
+}
+
+//El cierre de php no se incluye por que
+//el script debe ser incluido en otro 
